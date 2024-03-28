58d949e9a37d3008a5414a7869e175f0c727b1f5
SyedaAyeshaQazi <160398956+SyedaAyeshaQazi@users.noreply.github.com>
2024-03-28 20:34:59 +0500

    first commit
    This is CLI based number guessing game

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor((Math.random()) * 5 + 1);
+const answeres = await inquirer.prompt([
+    {
+        name: "yourguessnumber",
+        type: "number",
+        message: "please! guess a number between 1-5: ",
+    },
+]);
+if (answeres.yourguessnumber === randomnumber) {
+    console.log("Congratulations! you guessed a right number");
+}
+else {
+    console.log("Sorry! you guessed a wrong number");
+}
